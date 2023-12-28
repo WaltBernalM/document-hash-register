8013b3013f3643cde39c1b3c20a8602c749faba4
Walter Bernal <wltrbm@gmail.com>
2023-12-28 14:00:24 -0700

    transition to trycatch block in write_network_identity_cert function

--- write_network_identity_cert.py
+++ write_network_identity_cert.py
@@ -2,7 +2,10 @@ from azure.confidentialledger.certificate import ConfidentialLedgerCertificateCl
 
 
 def write_network_identity_cert(ledger_tls_cert_file_name: str, ledger_name: str, identity_url: str) -> None:
-  identity_client = ConfidentialLedgerCertificateClient(identity_url)
-  network_identity = identity_client.get_ledger_identity(ledger_id=ledger_name)
-  with open(ledger_tls_cert_file_name, "w") as cert_file:
-    cert_file.write(network_identity['ledgerTlsCertificate'])
+  try: 
+    identity_client = ConfidentialLedgerCertificateClient(identity_url)
+    network_identity = identity_client.get_ledger_identity(ledger_id=ledger_name)
+    with open(ledger_tls_cert_file_name, "w") as cert_file:
+      cert_file.write(network_identity['ledgerTlsCertificate'])
+  except Exception as e:
+    print(f"Error creating network_certificate.pem file: {e}")
